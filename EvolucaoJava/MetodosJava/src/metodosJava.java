d9d3791d18bbebf64b77b5515d089f68c1140636
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-05-11 09:23:08 -0300

    Aula Métodos Java
    Atividade feita para treinar utilização de métodos no JAVA

---
+++ EvolucaoJava/MetodosJava/src/metodosJava.java
@@ -0,0 +1,27 @@
+public class metodosJava {
+    //Criação dos metodos
+    public static void soma(int a,int b){
+        int resultado = a + b;
+        System.out.println("A SOMA DE A+B = "+ resultado);
+    }
+    public static void subtracao(int a,int b){
+        int resultado = a - b;
+        System.out.println("A SUBTRAÇÃO DE A+B = "+ resultado);
+    }
+    public static void divisao(int a,int b){
+        int resultado = a / b;
+        System.out.println("A DIVISÃO DE A+B = "+ resultado);
+    }
+    public static void multiplicacao(int a,int b){
+        int resultado = a * b;
+        System.out.println("A MULTIPLICAÇÃO DE A+B = "+ resultado);
+    }
+    // Função main, utilização dos métodos.
+    public static void main(String[] args){
+        soma(2,3);
+        subtracao(2,3);
+        divisao(2,3);
+        multiplicacao(2,3);
+    }
+
+}
