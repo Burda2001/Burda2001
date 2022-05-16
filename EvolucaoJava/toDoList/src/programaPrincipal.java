aa3c860c49d208d4eef6e47a99593bd78f23e034
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-05-16 16:12:59 -0300

    Ideia principais funcionalidades

--- EvolucaoJava/toDoList/src/programaPrincipal.java
+++ EvolucaoJava/toDoList/src/programaPrincipal.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class programaPrincipal {
@@ -8,6 +9,19 @@ public class programaPrincipal {
         System.out.println("1. Criar nova tarefa");
         System.out.println("2. Marcar tarefa como concluida");
         System.out.println("3. Excluir tarefa");
+        int opcao = input.nextInt();
+        String[] nomeTarefa;
+        switch (opcao){
+            case 1:
+                System.out.println("Qual o nome da tarefa?");
+                break;
+            case 2:
+                System.out.println("Qual tarefa concluir?");
+                break;
+            case 3:
+                System.out.println("Qual tarefa excluir?");
+                break;
+                        }
 
     }
 }
