50f6985e2f63158b24c3880692fc93261f45f837
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-05-16 15:53:38 -0300

    Criação ideia menu principal

--- EvolucaoJava/toDoList/src/programaPrincipal.java
+++ EvolucaoJava/toDoList/src/programaPrincipal.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class programaPrincipal {
-    
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Escolha a opção desejada:");
+        System.out.println("1. Criar nova tarefa");
+        System.out.println("2. Marcar tarefa como concluida");
+        System.out.println("3. Excluir tarefa");
+
+    }
 }
